6bbf3176ad9e9fb7dae673ef704b6460bb51a7cb
samobutniy-1 <nikitaterentev916@gmail.com>
2026-02-08 11:44:34 +0100

    Create Header component

---
+++ FoxShop/src/header/Header.jsx
@@ -0,0 +1,36 @@
+export function Header() {
+  return (
+    <>
+      <header className="header">
+        <div className="header__container">
+          <a href="/" className="header__logo-container">
+            <p className="header__logo">FoxShop</p>
+            <img className="header__img" src="/fox-logo.png" alt="logo" />
+          </a>
+          <input
+            type="text"
+            className="header__input"
+            placeholder="I am looking for.."
+          />
+          <div className="header__buttons">
+            <button type="button" className="header__cart-open-button">
+              <svg className="header__icon-cart">
+                <use href="svg/sprite.svg#cart"></use>
+              </svg>
+              <span className="indicator"></span>
+            </button>
+            <button
+              type="button"
+              className="header__burger burger"
+              // onclick="openSidebar()"
+            >
+              <span className="burger__line"></span>
+              <span className="burger__line burger__line--short"></span>
+              <span className="burger__line"></span>
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
